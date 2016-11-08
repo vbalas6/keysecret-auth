751c1dac5a26d3d944113e5d0f475fab3be443a7
Baladendapani <261128@CTS.COM>
2016-11-08 13:29:34 +0530

    PUL-2728_GenericAppKey

--- dist/browser.js
+++ dist/browser.js
@@ -10,12 +10,13 @@ keySecret.client = {
     header: function header(uri, method, options) {
 
         //  Remap key/secret for hawk
+				var appKey = options.appKey || 'KeySecret Key';
         options.credentials.id = options.credentials.key;
         options.credentials.key = options.credentials.secret;
 
         var hawkHeader = hawk.client.header(uri, method, options)
             //  Reformat into key/secret format
-        hawkHeader.field = hawkHeader.field.replace('Hawk id=', 'KeySecret key=')
+        hawkHeader.field = hawkHeader.field.replace('Hawk id=', appKey + '=')
         return hawkHeader;
     }
 }
